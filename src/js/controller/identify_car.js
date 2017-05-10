2a23e43e96f272c6a66df6a7d13ee4d384c1f825
zhudm <zhudm@chetong.net>
2017-05-10 16:12:03 +0800

    识别界面修改

--- src/js/controller/identify_car.js
+++ src/js/controller/identify_car.js
@@ -3,12 +3,21 @@ angular.module("ctApp")
 		"$scope",
 		"$state",
 		"$apis",
-		function($scope, $state, $apis) {
+		"$timeout",
+		function($scope, $state, $apis, $timeout) {
 
-			$scope.prefix = "";
-
-			$scope.send = function(carno){
+			$scope.identifyFailed = false;
+			$scope.send = function(carno) {
 				alert(carno);
-			}
+			};
+
+			var loading = null;
+			$scope.uploadImg = function() {
+				loading = weui.loading("正在识别...");
+				$timeout(function(){
+					$scope.identifyFailed = true;
+					loading.hide();
+				},2000);
+			};
 		}
 	]);

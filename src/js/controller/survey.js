0017b972397509320b80818ce38a2e0b64e9401d
zhudm <zhudm@chetong.net>
2017-05-11 12:04:24 +0800

    上传图片页面修改

--- src/js/controller/survey.js
+++ src/js/controller/survey.js
@@ -1,11 +1,49 @@
 angular.module("ctApp")
-    .controller("surveyEdit", [
-        "$scope",
-        "$state",
-        "$apis",
-        "$timeout",
-        function($scope, $state, $apis, $timeout) {
+	.controller("surveyEdit", [
+		"$scope",
+		"$state",
+		"$apis",
+		"$timeout",
+		function($scope, $state, $apis, $timeout) {
 
+			$scope.closeTip = function() {
+				$(".photo-tip").fadeOut();
+			};
 
-        }
-    ]);
+			$scope.choiceType = function() {
+				weui.actionSheet([{
+					label: '被别人划了，有划痕',
+					onClick: function() {
+						$("#type-text").text('被别人划了，有划痕');
+					}
+				}, {
+					label: '车头、尾撞到物体（墙、树、物体）',
+					onClick: function() {
+						$("#type-text").text('车头、尾撞到物体（墙、树、物体）');
+					}
+				}, {
+					label: '车侧面被撞伤',
+					onClick: function() {
+						$("#type-text").text('车侧面被撞伤');
+					}
+				}, {
+					label: '车顶部被砸伤',
+					onClick: function() {
+						$("#type-text").text('车顶部被砸伤');
+					}
+				}, {
+					label: '我也说不清',
+					onClick: function() {
+						$("#type-text").text('我也说不清');
+					}
+				}, ], [{
+					label: '取消',
+					onClick: function() {
+						$("#type-text").text('根据现场情况选择');
+					}
+				}], {
+					className: 'custom-classname'
+				});
+			};
+		}
+	]);

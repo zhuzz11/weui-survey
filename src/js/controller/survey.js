e16b37395d483e4bc474244191cce07a5c619ca2
zhudm <zhudm@chetong.net>
2017-05-12 14:32:56 +0800

    照片框改用grid，更易于与页面与数据交互

--- src/js/controller/survey.js
+++ src/js/controller/survey.js
@@ -51,72 +51,114 @@ angular.module("ctApp")
 
 
 			$scope.surveyPhoto = [{
-				left: {
-					desc: "全车45含清晰车牌照", //描述
-					descPhotoUrl: "images/car.png", //描述图片地址
-					uploading: true,
-					process: {
-						width: "20%"
-					}, //进度
-					uploaded: false,
-					auditPass: true,//审核是否通过
-					auditReturn: ""//退回原因
-				},
-				right: {
-					desc: "车辆损失部位",
-					descPhotoUrl: "images/car.png",
-					uploading: true,
-					process: {
-						width: "0%"
-					},
-					uploaded: false,
-					auditPass: false,
-					auditReturn: "“碰撞物体”拍摄不清晰，请重新拍摄上传"
-				}
+				desc: "全车45含清晰车牌照", //描述
+				descPhotoUrl: "images/car.png", //描述图片地址
+				uploading: true,
+				process: {
+					width: "20%"
+				}, //进度
+				uploaded: false,
+				auditPass: true, //审核是否通过
+				auditReturn: "" //退回原因
 			}, {
-				left: {
-					desc: "碰撞物体",
-					descPhotoUrl: "images/car.png",
-					uploading: true,
-					process: {
-						width: "40%"
-					},
-					uploaded: false
+				desc: "车辆损失部位",
+				descPhotoUrl: "images/car.png",
+				uploading: true,
+				process: {
+					width: "0%"
 				},
-				right: {
-					desc: "车架号",
-					descPhotoUrl: "images/car.png",
-					uploading: false,
-					process: {
-						width: "0%"
-					},
-					uploaded: false
-				}
+				uploaded: false,
+				auditPass: false,
+				auditReturn: "“碰撞物体”拍摄不清晰，请重新拍摄上传"
+
+			}, {
+				desc: "碰撞物体",
+				descPhotoUrl: "images/car.png",
+				uploading: true,
+				process: {
+					width: "40%"
+				},
+				uploaded: false
+			}, {
+				desc: "车架号",
+				descPhotoUrl: "images/car.png",
+				uploading: false,
+				process: {
+					width: "0%"
+				},
+				uploaded: false
 			}];
 
-			var t = $interval(function(){
-				var width = parseInt($scope.surveyPhoto[0].left.process.width);
-				if(width >= 100){
+			$scope.certPhoto = [{
+				desc: "行驶证", //描述
+				descPhotoUrl: "images/cert.png", //描述图片地址
+				uploading: true,
+				process: {
+					width: "20%"
+				}, //进度
+				uploaded: false,
+				auditPass: true, //审核是否通过
+				auditReturn: "" //退回原因
+			}, {
+				desc: "驾驶证",
+				descPhotoUrl: "images/cert.png",
+				uploading: true,
+				process: {
+					width: "0%"
+				},
+				uploaded: false,
+				auditPass: false,
+				auditReturn: "“碰撞物体”拍摄不清晰，请重新拍摄上传"
+
+			}, {
+				desc: "被保险人银行卡",
+				descPhotoUrl: "images/cert.png",
+				uploading: true,
+				process: {
+					width: "40%"
+				},
+				uploaded: false
+			}, {
+				desc: "被保险人身份证(正面)",
+				descPhotoUrl: "images/cert.png",
+				uploading: false,
+				process: {
+					width: "0%"
+				},
+				uploaded: false
+			}, {
+				desc: "被保险人银行卡(反面)",
+				descPhotoUrl: "images/cert.png",
+				uploading: false,
+				process: {
+					width: "0%"
+				},
+				uploaded: false
+			}];
+
+			var t = $interval(function() {
+				var width = parseInt($scope.surveyPhoto[0].process.width);
+				if (width >= 100) {
 					$interval.cancel(t);
 					t = null;
-					$scope.surveyPhoto[0].left.uploaded = true;
-					$scope.surveyPhoto[0].left.uploading = false;
+					$scope.surveyPhoto[0].uploaded = true;
+					$scope.surveyPhoto[0].uploading = false;
 					return;
 				}
-				$scope.surveyPhoto[0].left.process.width = width + 20 + "%";
-			},1000);
+				$scope.surveyPhoto[0].process.width = width + 20 + "%";
+			}, 1000);
 
 
-			var t2 = $interval(function(){
-				var width = parseInt($scope.surveyPhoto[0].right.process.width);
-				if(width >= 100){
+			var t2 = $interval(function() {
+				var width = parseInt($scope.surveyPhoto[1].process.width);
+				if (width >= 100) {
 					$interval.cancel(t2);
 					t2 = null;
-					$scope.surveyPhoto[0].right.uploaded = true;
-					$scope.surveyPhoto[0].right.uploading = false;
+					$scope.surveyPhoto[1].uploaded = true;
+					$scope.surveyPhoto[1].uploading = false;
 					return;
 				}
-				$scope.surveyPhoto[0].right.process.width = width + 10 + "%";
-			},1200);
+				$scope.surveyPhoto[1].process.width = width + 10 + "%";
+			}, 1200);
 		}
 	]);
